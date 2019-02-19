aa4182714a3460140917a1767e698626f37d6452
Jack Rosenthal <jack@rosenth.al>
2019-02-18 21:06:14 -0700

    Add accidentally-ignored files

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ for arg in "$@"; do
                 sudo apt-get install $(cat ~/dotfiles/debian-deps.txt)
             elif command -v pacman >/dev/null; then
                 sudo pacman -Sy $(cat ~/dotfiles/arch-deps.txt)
-                ( cd pkgbuild/xsecurelock && makepkg -c && sudo pacman -U *.pkg.tar.xz )
+                ( cd ~/dotfiles/pkgbuild/xsecurelock && makepkg -c && sudo pacman -U *.pkg.tar.xz )
             else
                 echo "WARNING: could not recognize system"
             fi
