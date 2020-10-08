8b6ee52f3ffc85da9be298eca3d339a0af664be8
Jack Rosenthal <jrosenth@chromium.org>
2020-10-08 13:57:47 -0600

    emacs: Update custom.el

--- emacs/custom.el
+++ emacs/custom.el
@@ -3,18 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(clang-format-executable "/usr/bin/clang-format")
  '(column-number-mode t)
  '(debug-on-error nil)
- '(hl-paren-background-colors (quote ("gainsboro")))
- '(hl-paren-colors (quote ("dark cyan")))
- '(magit-bury-buffer-function (quote magit-mode-quit-window))
+ '(hl-paren-background-colors '("gainsboro"))
+ '(hl-paren-colors '("dark cyan"))
+ '(magit-bury-buffer-function 'magit-mode-quit-window)
  '(paren-face-modes
-   (quote
-    (lisp-mode emacs-lisp-mode lisp-interaction-mode ielm-mode scheme-mode inferior-scheme-mode clojure-mode cider-repl-mode nrepl-mode arc-mode inferior-arc-mode racket-mode)))
+   '(lisp-mode emacs-lisp-mode lisp-interaction-mode ielm-mode scheme-mode inferior-scheme-mode clojure-mode cider-repl-mode nrepl-mode arc-mode inferior-arc-mode racket-mode))
  '(paren-face-regexp "[][()]")
  '(whitespace-style
-   (quote
-    (face trailing tabs lines-tail empty space-before-tab tab-mark))))
+   '(face trailing tabs lines-tail empty space-before-tab tab-mark)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
