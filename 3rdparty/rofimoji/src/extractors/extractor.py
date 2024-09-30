6b80961ffc14ad9d89c23a91a2f3c87599443140
Jack Rosenthal <jack@rosenth.al>
2024-09-29 22:59:13 -0600

    Remove 3rd party python projects

--- 3rdparty/rofimoji/src/extractors/extractor.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-
-class Extractor:
-    def extract_to(self, target: Path) -> None:
-        pass
--- 3rdparty/rofimoji/src/picker/__init__.py
+++
@@ -1,5 +0,0 @@
-from importlib import metadata
-
-__version__ = metadata.version("rofimoji")
-
-del metadata
